bb99e81f429fd9048d5c96a68f54a0bfd409cd40
SpencerDodd <spencer.evan.dodd@gmail.com>
2017-01-10 15:11:55 -0500

    Centralized project constants to `constants.py`. Formatting.

--- src/constants.py
+++ src/constants.py
@@ -1,8 +1,15 @@
-from peer import Peer
-import unittest
 """
 Constants used across the project
 """
+
 PROTOCOL_STRING = "BitTorrent protocol"
 ERROR_BYTESTRING_CHUNKSIZE = "Input not divisible by chunk size"
 MAX_PEERS = 1
+
+# Client information
+CLIENT_ID_STRING = "CO"
+CURRENT_VERSION = "0001"
+
+# Networking
+LISTENING_PORT_MIN = 6881
+LISTENING_PORT_MAX = 6889
--- src/core.py
+++ src/core.py
@@ -1,21 +1,13 @@
 import os
-import time
-import urllib
 import hashlib
 import unittest
-from torrent import Torrent
+from constants import CLIENT_ID_STRING, CURRENT_VERSION
 
-# Client information
-CLIENT_ID_STRING = "CO"
-CURRENT_VERSION = "0001"
-
-# Networking
-LISTENING_PORT_MIN = 6881
-LISTENING_PORT_MAX = 6889
-MAX_CONNECTIONS = 4
 """
 This is the core of the torrent client.
 """
+
+
 class Core:
 	def __init__(self):
 		"""
@@ -27,13 +19,12 @@ class Core:
 		self._active_torrents = {}
 		self._peer_id = self.generate_peer_id()
 
-	"""
-	Outputs a unique 20-byte urlencoded string used as the client identifier
-
-	Uses Azureus-style encoding:
-		'-' + (2-char client ID ascii) + (4-char integer version number) + '-'
-	"""
 	def generate_peer_id(self):
+		"""
+		Outputs a unique 20-byte urlencoded string used as the client identifier
+		Uses Azureus-style encoding:
+			'-' + (2-char client ID ascii) + (4-char integer version number) + '-'
+		"""
 		seed_string = "{}{}{}".format(os.getpgid(0), os.getcwd(), os.getlogin())
 		pre_versioned_peer_id = hashlib.sha1(seed_string).hexdigest()
 		peer_id_sub = pre_versioned_peer_id[28:]
@@ -43,6 +34,7 @@ class Core:
 """
 Tests
 """
+
 
 class TestClient(unittest.TestCase):
 	def test_peer_id_generation(self):
